f3808fd32fbf60154139c3846158a1fcbb65f95d
gabriel feijó vilela <61162949+vilelagabrielx@users.noreply.github.com>
2023-01-04 21:40:12 -0300

    Create mensagem.js
    Tratamento das mensagens no banco pelo sequelize

---
+++ src/mensagem.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const database = require('./conn').default;
+const Mensagem = database.define('t_mensagen',{
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    user: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    mensagem: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    data: {
+        type: Sequelize.STRING,
+        allowNull: true
+    }
+
+},{
+    timestamps: false
+})
+
+
+
+module.exports = Mensagem
